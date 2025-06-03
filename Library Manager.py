797720b529648fbdbc812ead1831ff7e25b32924
kimura kazusa <kaihatsujitsumuensyuu@gmail.com>
2025-06-03 09:32:05 +0900

    未読に変更

--- Library Manager.py
+++ Library Manager.py
@@ -1,5 +1,5 @@
 # Library Manager
-本や資料を簡単に管理するツール。タイトル・著者・ステータス（未読/読了/途中）を登録・編集・削除が可能。
+# 本や資料を簡単に管理するツール。タイトル・著者・ステータス（未読/読了/途中）を登録・編集・削除が可能。
 
 library = [
     {"title": "ワンピース", "author": "尾田栄一郎", "status": "読了"},
@@ -8,6 +8,6 @@ library = [
     {"title": "ジョジョの奇妙な冒険", "author": "荒木飛呂彦", "status": "途中"},
     {"title": "進撃の巨人", "author": "諫山創", "status": "読了"},#hata
     {"title": "東京リベンジャーズ", "author": "和久井健", "status": "未読"},#hata
-    {"title": "スラムダンク", "author": "井上井恵", "status": "読了"},#hata
+    {"title": "スラムダンク", "author": "井上井恵", "status": "未読"},#hata
     {"title": "", "author": "", "status": ""},
 ]
